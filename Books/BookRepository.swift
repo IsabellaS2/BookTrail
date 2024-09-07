bcab3bfa72ac0f182d66e70a1da97d1d54e76751
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2024-09-07 12:21:00 +0100

    Add API and get it to show on home screen

---
+++ Books/BookRepository.swift
@@ -0,0 +1,76 @@
+//
+//  BookRepository.swift
+//  Books
+//
+//  Created by Isabella Sulisufi on 06/09/2024.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case decodingError
+}
+
+class BookRepository {
+    func fetchBookRepo() async throws -> [Book] {
+        let endpoint = "https://gutendex.com/books/"
+        
+        guard let url = URL(string: endpoint) else {
+            throw NetworkError.invalidURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NetworkError.invalidResponse
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let booksResponse = try decoder.decode(BooksResponse.self, from: data)
+            return booksResponse.results
+            
+        } catch {
+            throw NetworkError.invalidData
+        }
+    }
+}
+
+//struct NetworkService {
+//    
+//    func fetchRepo(userName: String) async throws -> [Repository] {
+//        
+//        guard let url = URL(string: "https://api.github.com/users/\(userName)/repos") else {
+//            throw NetworkError.invalidURL
+//        }
+//        do {
+//            let (data, _) = try await URLSession.shared.data(from: url)
+//            let repositories = try JSONDecoder().decode([Repository].self, from: data)
+//            return repositories
+//        } catch {
+//            throw NetworkError.decodingError
+//        }
+//    }
+//}
+//
+//class GitHubViewModel {
+//        
+//    let networkService: NetworkService
+//    
+//    init(networkService: NetworkService) {
+//        self.networkService = networkService
+//    }
+//    
+//    func presentRepositories() async {
+//        do {
+//            let repositories = try await self.networkService.fetchRepo(userName: "pointfreeco")
+//            print(repositories.map { $0.name })
+//        } catch {
+//            print("Error: \(error)")
+//        }
+//    }
+//}
